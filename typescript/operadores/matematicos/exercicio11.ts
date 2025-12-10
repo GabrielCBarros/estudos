ca08e94f58b532efd587c36381d4bae51d996302
Gabriel Cerqueira Barros <gabrielcbarros11@gmail.com>
2025-12-10 12:29:30 -0300

    feat: operadores matemáticos e lógicos: Questão 11 type Script

---
+++ typescript/operadores/matematicos/exercicio11.ts
@@ -0,0 +1,18 @@
+//11. Informe um número e imprima o antecessor e o sucessor
+
+function imprimirAntecessorSucessor(){
+   return [antecessor, sucessor]
+    
+}
+let numero11: number = 10;
+let antecessor = numero11 - 1;
+let sucessor = numero11 + 1;
+
+
+ 
+let resultado11: number [] = imprimirAntecessorSucessor()
+ 
+ function imprimirResultado(resultado11: number []){
+    console.log(resultado11)
+ }
+  imprimirResultado(resultado11)
