7f326b002ede1274e1a4fbffb36b3e3716178beb
Gabriel Cerqueira Barros <gabrielcbarros11@gmail.com>
2025-12-10 12:27:27 -0300

    feat: operadores matemáticos e lógicos: Questão 12

---
+++ javascript/condicional/exercicio_01/operadores/matematicos/logicos/exercicio12.js
@@ -0,0 +1,15 @@
+//12. Informe um número e imprima com reajuste de +5% e -5%
+
+function numeroReajuste(){
+    return [menos,mais]
+}
+let numero = 20;
+let menos = numero + (numero * 0.05);
+let mais = numero - (numero * 0.05);
+
+let resultado = numeroReajuste()
+
+function imprimirResultado(resultado){
+    console.log(resultado)
+}
+imprimirResultado(resultado)
---
+++ typescript/operadores/matematicos/exercicio12.ts
@@ -0,0 +1,15 @@
+//12. Informe um número e imprima com reajuste de +5% e -5%
+
+function numeroReajuste(){
+    return [menos,mais]
+}
+let numero = 20;
+let menos = numero + (numero * 0.05);
+let mais = numero - (numero * 0.05);
+
+let resultado12: number[] = numeroReajuste()
+
+function imprimirResultado(resultado12: number[]){
+    console.log(resultado12)
+}
+imprimirResultado(resultado12)
