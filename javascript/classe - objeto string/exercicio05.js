f33f224441d2b994cf5c4e7a73ee7b1821ba4522
Gabriel Cerqueira Barros <gabrielcbarros11@gmail.com>
2026-01-16 10:50:47 -0300

    feat: Classes/ objetos string: Questão 05

---
+++ javascript/classe - objeto string/exercicio05.js
@@ -0,0 +1,6 @@
+//5. Informe seu nome e verifique se ele contém a sequência de caracteres “br”
+
+let nome = "gabriel";
+let contemBr = nome.includes("br")
+
+console.log(contemBr)
---
+++ typescript/classe - objeto string/exercicio05.ts
@@ -0,0 +1,6 @@
+//5. Informe seu nome e verifique se ele contém a sequência de caracteres “br”
+export {};
+let nome: string = "gabriel";
+let contemBr: boolean = nome.includes("br")
+
+console.log(contemBr)
