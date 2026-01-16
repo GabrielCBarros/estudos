3b884d6604b402d450fc3b934ef92b3926a37e86
Gabriel Cerqueira Barros <gabrielcbarros11@gmail.com>
2026-01-16 15:44:59 -0300

    feat: Classes/ objetos string: Questão 10

---
+++ javascript/classe - objeto string/exercicio10.js
@@ -0,0 +1,8 @@
+//10. Informe seu nome e extraia as últimas 3 letras do seu nome
+
+let nome = "Gabriel";
+let ultimasTresLetras = nome[nome.length - 3] + 
+                        nome[nome.length - 2] +
+                        nome[nome.length - 1];
+
+console.log(ultimasTresLetras)
